00a5483029d832e2fb12ddecab36ba9366d0e119
Breta01 <breta.hajek@gmail.com>
2022-12-30 10:23:24 +0800

    Fix - loading test data

--- src/detection_algorithm.py
+++ src/detection_algorithm.py
@@ -10,36 +10,35 @@ sns.set_theme()
 
 
 def main():
-    config = TrainingConfig(target_column=-1)
+    config = TrainingConfig(data_type="csv", target_column=-1)
     model = load_model(config.custom_data_path, config.experimental)
     # Load data and train model
     X, y = load_data(config)
-    model.fit(X, y)
 
     # Dictionary mapping model name to calculated value (e.g. {"Mean": 10, ...})
     values = {m.model_name: m.predict(None) for m in model.models}
     print(values)
 
     threshold = 2
-    anomaly = abs((y - values["Mean"]) / values["Std"])
+    anomaly = abs((y - values["Mean"]) / values["Std"]) > threshold
 
     plt.figure(figsize=(10, 7))
     plt.plot(anomaly, label=f"Total anomalies: {sum(anomaly)}")
 
     nonzero = np.nonzero(anomaly)[0]
-    # plt.scatter(nonzero, np.ones_like(nonzero), c="r")
-    # for i, x in enumerate(nonzero):
-    #     plt.annotate(
-    #         f"{x}",
-    #         (x, 1),
-    #         textcoords="offset points",
-    #         xytext=(0, 2 + 10 * (i % 2)),
-    #         ha="center",
-    #     )
+    plt.scatter(nonzero, np.ones_like(nonzero), c="r")
+    for i, x in enumerate(nonzero):
+        plt.annotate(
+            f"{x}",
+            (x, 1),
+            textcoords="offset points",
+            xytext=(0, 2 + 10 * (i % 2)),
+            ha="center",
+        )
 
     plt.xlabel("Production Period")
     plt.ylabel("Anomaly")
-    # plt.yticks([0, 1], ["No", "Yes"])
+    plt.yticks([0, 1], ["No", "Yes"])
     plt.legend(title="Status", loc=6)
     plt.title("Anomaly status for each period")
     plt.savefig(config.output_folder / "result.jpg")
